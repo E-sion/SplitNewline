90076479002e2947a792f58ecb3ffc87757dbfe5
G-Sea <101786678+G-Sea@users.noreply.github.com>
2023-09-01 21:19:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,33 +8,36 @@ from pkg.plugin.host import EventContext, PluginHost
 # 注册插件
 @register(name="SplitNewline", description="多段回复", version="0.1", author="cillow")
 class SplitNewlinePlugin(Plugin):
-    # 定义一个函数，接受一个事件名和一个字典作为参数
-    # 当收到GPT回复时触发
+    
     
     # 插件加载时触发
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
         pass
+
+    # 定义一个函数，接受一个事件名和一个字典作为参数
+    # 当收到GPT回复时触发
     @on(NormalMessageResponded)
     def on_event(self, event: EventContext, **kwargs):
-        # 判断事件名是否是NormalMessageResponded
         # 获取事件参数中的响应文本
-        response_text = kwargs["response_text"]
-        # 判断响应文本是否包含换行符
-        if "/n" in response_text:
-            # 按换行符分割响应文本
-            parts = response_text.split("/n")
-            # 创建一个空列表，用来存储修改后的回复消息组件
-            reply = []
-            # 遍历每个部分
-            for part in parts:
-                # 把当前部分作为一个文字消息组件添加到回复列表中
-                reply.append({"type": "text", "data": {"text": part}})
-            # 返回修改后的回复列表，替换原始回复
-            return {"reply": reply}
-            #debug 输出内容
-            logging.debug("SplitNewline插件进入成功！"+reply)
+        response_text:str = kwargs['response_text']
 
+        # 判断响应文本是否包含换行符
+        def print_split(s):
+             # 用·符号分割字符串，得到一个列表
+            parts = s.split('·')
+             # 遍历列表中的每个元素
+            for part in parts:
+            # 打印元素
+                event.add_return("reply", [reply])
+        # 回复消息
+        print_split(response_text)
+        
+        # 阻止该事件默认行为（向接口获取回复）
+        event.prevent_default()
+        event.prevent_postorder()
+
+        
         
 
     # 插件卸载时触发
