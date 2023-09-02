43c912365f8bab4324ddbe66592ed6725fd51496
G-Sea <101786678+G-Sea@users.noreply.github.com>
2023-09-02 10:30:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
+import re
+from time import sleep
 
 """
 多段回复
 """
 
+
 # 注册插件
 @register(name="SplitNewline", description="多段回复", version="0.1", author="cillow")
 class SplitNewlinePlugin(Plugin):
-    
-    
+
     # 插件加载时触发
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
@@ -20,26 +22,22 @@ class SplitNewlinePlugin(Plugin):
     @on(NormalMessageResponded)
     def on_event(self, event: EventContext, **kwargs):
         # 获取事件参数中的响应文本
-        response_text:str = kwargs['response_text']
+        responses_text = kwargs['response_text']
 
-        # 判断响应文本是否包含换行符
-        def print_split(s):
-             # 用·符号分割字符串，得到一个列表
-            parts = s.split('·')
-             # 遍历列表中的每个元素
-            for part in parts:
-            # 打印元素
-                event.add_return("reply",part)
-        # 回复消息
-        print_split(response_text)
-        
-        # 阻止该事件默认行为（向接口获取回复）
+        pattern = "·"
+
+        parts = re.split(pattern, responses_text)
+
+        # 遍历列表中的每个元素
+        for msg in parts:
+            
+            # send_person_message
+            host: pkg.plugin.host.PluginHost = kwargs['host']
+            host.send_person_message(kwargs['launcher_id'], msg) if kwargs['launcher_type'] == 'person' else host.send_group_message(
+                kwargs['launcher_id'], msg)
         event.prevent_default()
         event.prevent_postorder()
-
         
-        
-
     # 插件卸载时触发
     def __del__(self):
         pass
